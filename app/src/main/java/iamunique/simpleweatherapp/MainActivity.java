4bef9400462629340f1cc541a0cc2df9f0d1cccf
mahsan87 <mahsan87@gmail.com>
2017-09-08 02:17:27 +0100

    Added find my location

--- app/src/main/java/iamunique/simpleweatherapp/MainActivity.java
+++ app/src/main/java/iamunique/simpleweatherapp/MainActivity.java
@@ -1,8 +1,21 @@
 package iamunique.simpleweatherapp;
 
+import android.Manifest;
 import android.app.ProgressDialog;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,20 +29,34 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Textview to show temperature and description
+    TextView temperature, description, cityName;
+    EditText city;
+    String enteredCity;
+    Button search, location;
+    private double lat, lon;
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private static final int MY_PERMISSION_ACCESS_COARSE_LOCATION = 11;
+
+    //String city= "Darmstadt";
+    //String country= "de";
+
     // we"ll make HTTP request to this URL to retrieve weather conditions
-    String weatherWebserviceURL = "http://api.openweathermap.org/data/2.5/weather?q=ariana,tn&appid=2156e2dd5b92590ab69c0ae1b2d24586&units=metric";
+    //String weatherWebserviceURL = "http://api.openweathermap.org/data/2.5/weather?q="+enteredCity+"&appid=61eb25f1560a6de656f3a3346bf2a956&units=metric";
 
     //the loading Dialog
     ProgressDialog pDialog;
-
-    // Textview to show temperature and description
-    TextView temperature, description;
 
     // background image
     ImageView weatherBackground;
@@ -37,26 +64,23 @@ public class MainActivity extends AppCompatActivity {
     // JSON object that contains weather information
     JSONObject jsonObj;
 
+    private void callService(final String webService){
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        weatherBackground = (ImageView) findViewById(R.id.weatherbackground);
 
-        //link graphical items to variables
-        temperature= (TextView) findViewById(R.id.temperature);
-        description= (TextView) findViewById(R.id.description);
-        weatherBackground= (ImageView) findViewById(R.id.weatherbackground);
+        Log.v("SERVICE", webService.toString());
+        //Log.v("EnteredCity", enteredCity.toString());
 
         // prepare and show the loading Dialog
-        pDialog= new ProgressDialog(this);
+        pDialog = new ProgressDialog(MainActivity.this);
         pDialog.setMessage("Please wait while retrieving weather conditions ..");
         pDialog.setCancelable(false);
         pDialog.show();
 
+
         // make HTTP request to retrieve the weather
         JsonObjectRequest jsonObjReq = new JsonObjectRequest(Request.Method.GET,
-                weatherWebserviceURL, null, new Response.Listener<JSONObject>() {
+                webService, null, new Response.Listener<JSONObject>() {
 
             @Override
             public void onResponse(JSONObject response) {
@@ -64,11 +88,11 @@ public class MainActivity extends AppCompatActivity {
                     // Parsing json object response
                     // response will be a json object
 
-
                     jsonObj = (JSONObject) response.getJSONArray("weather").get(0);
                     // display weather description into the "description textview"
                     description.setText(jsonObj.getString("description"));
                     // display the temperature
+                    cityName.setText(response.getString("name") + ", ");
                     temperature.setText(response.getJSONObject("main").getString("temp") + " °C");
 
                     String backgroundImage = "";
@@ -125,8 +149,63 @@ public class MainActivity extends AppCompatActivity {
                 pDialog.dismiss();
             }
         });
+        // Adding request to request queue
+        AppController.getInstance(MainActivity.this).addToRequestQueue(jsonObjReq);
+    }
 
-// Adding request to request queue
-        AppController.getInstance(this).addToRequestQueue(jsonObjReq);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        search = (Button) findViewById(R.id.search);
+        search.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //search city temperature
+
+                //link graphical items to variables
+                temperature = (TextView) findViewById(R.id.temperature);
+                description = (TextView) findViewById(R.id.description);
+                city = (EditText) findViewById(R.id.editText);
+                cityName= (TextView) findViewById(R.id.textView3);
+                enteredCity = city.getText().toString();
+                final String weatherWebserviceURL = "http://api.openweathermap.org/data/2.5/weather?q=" + enteredCity + "&appid=61eb25f1560a6de656f3a3346bf2a956&units=metric";
+                callService(weatherWebserviceURL);
+            }
+        });
+
+        //Get my location
+        location = (Button) findViewById(R.id.location);
+        location.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                mFusedLocationClient = LocationServices.getFusedLocationProviderClient(MainActivity.this);
+
+                if (ContextCompat.checkSelfPermission(MainActivity.this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION},
+                            MY_PERMISSION_ACCESS_COARSE_LOCATION);
+                }
+
+                mFusedLocationClient.getLastLocation()
+                        .addOnSuccessListener(MainActivity.this, new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                // Got last known location. In some rare situations this can be null.
+                                if (location != null) {
+                                    temperature = (TextView) findViewById(R.id.temperature);
+                                    description = (TextView) findViewById(R.id.description);
+                                    cityName= (TextView) findViewById(R.id.textView3);
+                                    lat= location.getLatitude();
+                                    lon= location.getLongitude();
+                                    Log.v("LATLONG", lat+" "+lon);
+
+                                    final String weatherWebserviceURL1 = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon="+ lon +"&appid=61eb25f1560a6de656f3a3346bf2a956&units=metric";
+                                    callService(weatherWebserviceURL1);
+                                }
+                            }
+                        });
+            }
+        });
     }
 }
